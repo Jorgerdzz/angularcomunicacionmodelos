a7b2fdfbb3582bb5c419d415f3b72e3d0c7e98d5
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2025-11-06 11:26:55 +0100

    Modelo producto

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -2,10 +2,12 @@ import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { App } from './app';
+import { Listaproductos } from './components/listaproductos/listaproductos';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Listaproductos
   ],
   imports: [
     BrowserModule
---
+++ src/app/models/Producto.ts
@@ -0,0 +1,13 @@
+export class Producto{
+    public nombre: string;
+    public imagen: string;
+    public precio: number;
+
+
+   constructor(nombre: string, imagen:string, precio:number){
+    this.nombre = nombre;
+    this.imagen = imagen;
+    this.precio = precio;
+   } 
+
+}
