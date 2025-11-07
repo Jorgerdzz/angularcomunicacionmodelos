02cc28395d6f4e7020e3e6ae693d4e0a0fe08234
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2025-11-07 10:47:05 +0100

    fin proyecto

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -14,6 +14,7 @@ import { ComicComponent } from './components/comic.component/comic.component';
 import { FormsModule } from '@angular/forms';
 
 import { ServiceComics } from './services/service.comics';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import { ServiceComics } from './services/service.comics';
   providers: [
     provideBrowserGlobalErrorListeners(),
     appRoutingProvider,
-    ServiceComics
+    ServiceComics,
+    provideHttpClient()
   ],
   bootstrap: [App]
 })
